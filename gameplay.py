36e994e261b0c04bdb155726c1630e7fc3839558
Abdulrahman Ehab <eng.abdulrahmanehab23@gmail.com>
2025-09-17 07:43:56 +0300

    feat: obstacle score , weapon upgrade

--- gameplay.py
+++ gameplay.py
@@ -29,7 +29,7 @@ class Player(pygame.sprite.Sprite):
         self.speed = 500
         self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self,dt , move=0):
+    def update(self,dt,move=0):
         self.direction.y=0  
         if move==1:
             self.direction.y=-1
@@ -45,33 +45,32 @@ class Player(pygame.sprite.Sprite):
         self.mask=pygame.mask.from_surface(self.image)
         
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self,groups,y_pos,y_speed,obstacle_type,width,hight):   #y_pos is the vertical position for the obstacle #y_speed is the speed in y axis 
+    def __init__(self,groups,y_pos,obstacle_type,width,hight):   #y_pos is the vertical position for the obstacle #y_speed is the speed in y axis 
         super().__init__(groups)
         #initialize the obstacle size,color,speed and direction.
         o_width , o_hight =64,64  #obstacle width and hight
+        self.obstacle_type = obstacle_type
         self.image = pygame.Surface((o_width,o_hight))
-        if obstacle_type == 1:
+        if self.obstacle_type == 1:
             sheet_bug=pygame.image.load(r"attachment\Scorpion.png").convert_alpha()
             self.surfaces_bug=creat_animation(8,(64,64),5,sheet_bug,True,1.5)
             self.i=0
             self.image=self.surfaces_bug[0]
-        elif obstacle_type == 2 :
+            self.y_speed = 0
+        elif self.obstacle_type == 2 :
             sheet_bug=pygame.image.load(r"attachment\Clampbeetle.png").convert_alpha()
             self.surfaces_bug=creat_animation(8,(64,64),5,sheet_bug,True,1.5)
             self.i=0
             self.image=self.surfaces_bug[0] 
+            self.y_speed = 300
         self.w_width = width
         self.w_hight = hight            
         if y_pos == None:
             y_pos=random.randint(o_hight//2,self.w_hight-2*o_hight)  # another obstacle will be under the first one , they must not enteract
         self.y_pos = y_pos    
         self.rect=self.image.get_rect(bottomleft=(-200,y_pos))
-        self.x_speed=  500 
-        if y_speed == None:  
-            y_speed = 0           
-        self.y_speed = y_speed
+        self.x_speed=  500           
         self.direction = pygame.Vector2(1,random.choice([-1,1])) #obstacle move to lift with random vertical initial direction 
-        self.mask = pygame.mask.from_surface(self.image)
 
     def check_y2(self,y2):  #check if y2 is not on the screen 
         if y2 > self.w_hight:    #y2 is the vertical coordinate for the second obstacle (if two obstacles get generated together)
@@ -89,7 +88,6 @@ class Obstacle(pygame.sprite.Sprite):
         self.rect.x += self.direction.x*self.x_speed* dt
         self.rect.y += self.direction.y*self.y_speed* dt 
         self.image, self.i = animat(dt,self.surfaces_bug,self.i)  
-        self.mask = pygame.mask.from_threshold(self.image,(0,0,0,255),(50,50,50,255))
   
         #the obstacle dissapper when get out of the screen and seperate it for my waves of enemy to increase numbers of enemy in the wave 
                
@@ -100,21 +98,27 @@ class Obstacle(pygame.sprite.Sprite):
         else : 
             False    
 
-class Wipons(pygame.sprite.Sprite):
-    def __init__(self,player,groups,width,hight,surfaces):
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,player,groups,surfaces,bullet_direction):
         super().__init__(groups)
         self.surfaces=surfaces
         self.image=surfaces[0]
         self.i=0
         self.rect=self.image.get_rect(midright = player.rect.midleft)
-        self.speed = 1000
-        self.direction = pygame.Vector2(-1,0)
-        self.w_width = width
-        self.w_hight = hight
+        self.bullet_direction = bullet_direction
+        self.x_speed = 1000
+        self.y_speed = 180
+        if self.bullet_direction == 0:
+            self.direction = pygame.Vector2(-1,0)
+        if self.bullet_direction == 1:
+            self.direction = pygame.Vector2(-1,-1)    
+        if self.bullet_direction == -1:
+            self.direction = pygame.Vector2(-1,1)    
+    
 
     def update(self,dt):
-        keys = pygame.key.get_pressed()
-        self.rect.x += self.direction.x*self.speed*dt
+        self.rect.x += self.direction.x*self.x_speed*dt
+        self.rect.y += self.direction.y*self.y_speed*dt
         self.image, self.i = animat(dt,self.surfaces,self.i) 
         if self.rect.x <0:
             self.kill()    
@@ -131,21 +135,22 @@ class Collisions:
            dx = self.player.rect.centerx - obs.rect.centerx
            dy = self.player.rect.centery - obs.rect.centery
            d = (dx**2 + dy**2)**0.5 #distance
-           collision_radius = 50 #this number represent the min distance between the player and obstacle before git collision
+           collision_radius = 40 #this number represent the min distance between the player and obstacle before git collision
            if d<collision_radius :
                return True
            else:
                return False
 
-
-    
-    def bullet_collision(self,all_sprites): #check if bullet collide to an obstacle
+    def bullet_collision(self,all_sprites,obstacle): #check if bullet collide to an obstacle
         hits = pygame.sprite.groupcollide(self.shoots,self.obstacles,True,True) #if no collision happened , hits equal to zero
         if hits:
             self.hit=1 
             for bullet,obstacles in hits.items():
                 pos = obstacles[0].rect.center
-                float_text = Floating_text("+5",pos,all_sprites) #display +5 score when bullet collide to obstacle
+                if obstacle.obstacle_type == 1:
+                    float_text = Floating_text("+3",pos,all_sprites) #isplay +3
+                elif obstacle.obstacle_type == 2:
+                    float_text = Floating_text("+5",pos,all_sprites) #display +5 score when bullet collide to obstacle   
             return True #it for my waves of enemy to increase numbers of enemy in the wave
         else :     
             self.hit=0
@@ -154,30 +159,33 @@ class Collisions:
 class Score_counter:
     def __init__(self,width,hight,surf):
         self.total_score = 0 
-        self.time_score=0
-        self.hit_score=[] 
+        self.hit_score_type1=[] 
+        self.hit_score_type2=[]
         self.w_width = width
         self.w_hight = hight
         self.board_counter=pygame.image.load(r"attachment\empty_window_show_score.png").convert_alpha()
         self.rect_board_counter=self.board_counter.get_rect(center = (self.w_width//2,self.w_hight-self.w_hight//8))
         self.display_surface = surf
 
-    def update_score(self,hit):
+    def update_score(self,hit,obstacle_type):
         self.display_surface.blit(self.board_counter ,self.rect_board_counter)
-        self.time_score = pygame.time.get_ticks()//500  #score increase by 2 every second
-        self.hit_score.append(1) if hit else 0
-        self.total_score =len(self.hit_score)*5  #score increase by 5 every correct hit
+        if hit and obstacle_type==1:
+            self.hit_score_type1.append(1)
+        if hit and obstacle_type==2:   
+            self.hit_score_type2.append(1) 
+        self.total_score =len(self.hit_score_type1)*3 + len(self.hit_score_type2)*5 #score increase by 5 every correct hit for obstacle type 2 , by 3 for type 1
         # i made 2 func in 1 instead of calling twice
         font = pygame.font.Font(r"attachment\jungle-adventurer\JungleAdventurer.otf",50)
         score_text = font.render(f"{self.total_score}",True,(221,243,231))
         text_rect = score_text.get_rect(center = (self.w_width//2,self.w_hight-self.w_hight//8))
         self.display_surface.blit(score_text,text_rect) # display the score within a rectangle
+        return self.total_score
 
 class Floating_text(pygame.sprite.Sprite): #display floating text
     def __init__(self,text,pos,groups):
         super().__init__(groups)
         font = pygame.font.Font(None,40)
-        self.image = font.render(text,True,(255,80,80))
+        self.image = font.render(text,True,(255,69,0))
         self.rect = self.image.get_rect(center = pos)
         self.lifetime = 60
 
@@ -221,6 +229,7 @@ def end_game(display_surface,w_width,w_hight, score,cap):
         pygame.display.update()
 
 def controller(mp_hands,hands,mp_draw,cap,c_width,c_hight):
+    #this function take the porocessed frame and convert it to hame actions
     frame,move,fire,pause=None,0,False,False
     keys = pygame.key.get_pressed()
     frame,stop,shoot,gesture = process_frame(mp_hands,hands,mp_draw,cap,c_width,c_hight)
@@ -239,14 +248,18 @@ def controller(mp_hands,hands,mp_draw,cap,c_width,c_hight):
         pause = True    
     return frame,move,fire,pause   
         
-def shooting(time_delay,last_shoot_time,player,group1,group2,surfaces_bullets):
+def shooting(time_delay,last_shoot_time,player,group1,group2,surfaces_bullets,your_score,threshold_score):
     now = pygame.time.get_ticks()
     if now - last_shoot_time > time_delay:
-        Wipons(player,[group1,group2],player.w_width,player.w_hight , surfaces_bullets) 
+        Bullet(player,[group1,group2],surfaces_bullets,0) 
+        if your_score >= threshold_score:
+            Bullet(player,[group1,group2],surfaces_bullets,1) 
+            Bullet(player,[group1,group2],surfaces_bullets,-1) 
+
         last_shoot_time = now
     return last_shoot_time 
 
-def creat_animation (num_frames ,size, row , surface_sheet, flip=False,factor_scale=False) :
+def creat_animation (num_frames,size,row,surface_sheet,flip=False,factor_scale=False) :
     surface_animation=[]
     for i in range(num_frames ) :
         rect=pygame.Rect(0+size[1]*i , 0+row*size[0] ,size[0] ,size[1])
@@ -264,6 +277,7 @@ def animat (dt ,surfaces, i) :
     return surface ,i
   
 def process_frame(mp_hands,hands,mp_draw,cap,c_width,c_hight):
+    # move this fun here from hand_travking_module to reduce camera startup time and make game faster
     font = cv2.FONT_HERSHEY_SIMPLEX
     while True:
         #read and flip the frame
@@ -305,7 +319,7 @@ def start_game(cap):
     #screen initialization
     w_width,w_hight = 1360,720#window width and hight
     display_surface = pygame.display.set_mode((w_width,w_hight))
-    pygame.display.set_caption("first prototype")
+    pygame.display.set_caption("good game")
     play_board=pygame.image.load(r"attachment\play_board.png").convert_alpha()
     player_back=pygame.image.load(r"attachment\BG_player.png")
     mp_hands,hands,mp_draw = ht.prepare_tracking()
@@ -320,13 +334,14 @@ def start_game(cap):
     player = Player(all_sprites,w_width-300,w_hight)
     collisions=Collisions(player,obstacles,shoots)
     score = Score_counter(w_width,w_hight,display_surface)
-    #your old spawn obsctale don't needed delete it
-    # obstacle_event = pygame.event.custom_type()
-    # pygame.time.set_timer(obstacle_event,300)
-    #those for my waves to increase smothly in number of enemy in one wave
+    #obstacles respone params : those for my waves to increase smothly in number of enemy in one wave
     num_in_wave=1 
     num_created=0
     all_dead=0
+    #params for upgrade weapon
+    your_score=0
+    threshold_score=50
+    show_upgrade_text=True
     #other parameters
     last_shoot_time=0
     time_delay=500 #delay between every single shoot
@@ -334,45 +349,43 @@ def start_game(cap):
     FPS = 60
     running = True
     just_unpaused = False
-    y_speed=300
     while running:
         #delta time 
         dt = clock.tick(FPS)/1000
         if just_unpaused and dt > 0.06:  # Skip large dt after unpause
             dt = 0
             just_unpaused = False
-        ###########
+        #take game actions params from controller fun
         frame,move,fire,pause = controller(mp_hands,hands,mp_draw,cap,300,220)
         #events
         for event in pygame.event.get():
+            #quit game
             if event.type == pygame.QUIT:
                 running = False
                 return False,0 
+            #pause game
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE :
-                    now = pygame.time.get_ticks()
-                    if now - last_shoot_time > time_delay:
-                        shoot = Wipons(player,[all_sprites,shoots],w_width,w_hight,surfaces_bullets) 
-                        last_shoot_time = now
                 if event.key == pygame.K_ESCAPE:
                     running= Main_window.pause(display_surface, w_width,w_hight,cap)
                     just_unpaused = True
                     if not running :
                         return False,0 
+        #respone obstacles            
         if num_created < num_in_wave  : # it for creat enemys untel reach the max number in the and don't others untel all enemies in brevious wave dead
             chance = random. randint(0,3) #random generator to detect the type of obstacle that will be generated every single event , 50% chance to generate one obstacle
             if chance == 3:
                 y_speed = 300
-                obstacle2 = Obstacle([all_sprites,obstacles],None,y_speed,2,w_width-320,w_hight)
+                obstacle = Obstacle([all_sprites,obstacles],None,2,w_width-320,w_hight)
             elif chance : 
-                obstacle = Obstacle([all_sprites,obstacles],None,None,1,w_width-320,w_hight)
+                obstacle = Obstacle([all_sprites,obstacles],None,1,w_width-320,w_hight)
             else :
-                obstacle = Obstacle([all_sprites,obstacles],None,None,1,w_width-320,w_hight)   
+                obstacle = Obstacle([all_sprites,obstacles],None,1,w_width-320,w_hight)   
                 y2 = obstacle.y_pos+random.randint(100,400) #the distance between the two obstacles
                 y2 = obstacle.check_y2(y2)  
-                obstacle = Obstacle([all_sprites,obstacles],y2,None,1,w_width-320,w_hight)
+                obstacle = Obstacle([all_sprites,obstacles],y2,1,w_width-320,w_hight)
+        # fire        
         if fire:
-            last_shoot_time = shooting(time_delay,last_shoot_time,player,all_sprites,shoots , surfaces_bullets)
+            last_shoot_time = shooting(time_delay,last_shoot_time,player,all_sprites,shoots,surfaces_bullets,your_score,threshold_score)
         #player moving
         player.update(dt,move)
         #pause
@@ -383,9 +396,9 @@ def start_game(cap):
                 return False,0 
         #collisions
         if collisions.check_collisions():
-            checker=end_game(display_surface,w_width,w_hight , score.total_score,cap)
+            checker=end_game(display_surface,w_width,w_hight,score.total_score,cap)
             return checker
-        checker_for_killed_enemy =collisions.bullet_collision(all_sprites)
+        checker_for_killed_enemy =collisions.bullet_collision(all_sprites,obstacle)
         if checker_for_killed_enemy or any( obs.check_passing_screen() for obs in obstacles): # to count for obscals that are  dead or pass 
             all_dead +=1
         if all_dead ==num_in_wave :
@@ -393,6 +406,10 @@ def start_game(cap):
             num_created = 0 
         elif num_created < num_in_wave :
             num_created += 1
+        #upgrade-weapon text
+        if your_score >= threshold_score and show_upgrade_text:
+           float_text2 = Floating_text("weapon upgraded!!",(500,500),all_sprites)
+           show_upgrade_text=False
         #update and display screen
         display_surface.blit(player_back,(1030,-20))    
         display_surface.blit(play_board, (-240,0))
@@ -404,7 +421,7 @@ def start_game(cap):
         all_sprites.update(dt)
         all_sprites.draw(display_surface)
         #update score
-        score.update_score(collisions.hit) #collisions.hit is 1 if a bullet hit an obstacle, else 0
+        your_score = score.update_score(collisions.hit,obstacle.obstacle_type) #collisions.hit is 1 if a bullet hit an obstacle, else 0
         pygame.display.update()
 
 #pygame.quit() >>> not needed , already called in the menu module
